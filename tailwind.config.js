439f8672dd169ee878a31a62b83dccd79e2dc69d
Atcheri Atch <atcheri@gmail.com>
2022-11-22 13:32:41 +0100

    feat: add flowbite to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./sections/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
   ],
   darkMode: "class",
   theme: {
@@ -13,5 +15,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwind-gradient-mask-image")],
+  plugins: [
+    require("tailwind-gradient-mask-image"),
+    require("flowbite/plugin"),
+  ],
 };
