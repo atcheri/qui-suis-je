19bdf71896862c2d36c761096fbdccbe29af37e7
Atcheri Atch <atcheri@gmail.com>
2022-11-23 23:13:14 +0100

    fix: trying getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
 import { FC, useCallback, useEffect } from "react";
@@ -56,7 +56,7 @@ const Home: FC = () => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
     props: {
       ...(await serverSideTranslations(locale || "en", [
