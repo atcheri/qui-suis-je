1810168a90315304b7510afda31be02617559332
atcheri <atcheri@gmail.com>
2022-12-09 16:53:15 +0100

    feat: additional meta tags in header

--- sections/Layout.tsx
+++ sections/Layout.tsx
@@ -5,12 +5,22 @@ import Header from './Header';
 import Footer from './Footer';
 
 const Layout: FC<PropsWithChildren<{}>> = ({ children }) => {
+  const title = 'Qui-sui je? Atch';
+  const description = "Non, ce n'est pas un site de rencontre.";
   return (
     <>
       <Head>
-        <title>Qui-sui je? Atsuhiro</title>
-        <meta name="description" content="Non, ce n'est pas un site de rencontre." />
-        <link rel="icon" href="/favicon.ico" />
+        <title>{title}</title>
+        <meta property="og:title" content="A-T-C-H" key="title" />
+        <meta name="description" content={description} />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="mask-icon" href="/itsutsu-inazuma.svg" color="#362F78"></link>
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="/qui-suis-je-hero.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+        <link rel="manifest" href="/manifest.json" />
       </Head>
 
       <div className="min-h-screen mx-auto max-w-6xl flex flex-col">
