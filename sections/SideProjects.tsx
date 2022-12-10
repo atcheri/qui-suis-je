404e3eed018d0d857c63c15f3791462d8931db94
Atch <atcheri@gmail.com>
2022-12-10 15:12:48 +0100

    feat: add translations to the projects block

--- sections/SideProjects.tsx
+++ sections/SideProjects.tsx
@@ -1,18 +1,17 @@
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import AnchoredHeader from '../components/AnchoredHeader';
 import Section from '../components/Section';
 import { SECTION } from './constants';
 
 const SideProjects: FC = () => {
+  const { t } = useTranslation('common');
+
   return (
     <Section id={SECTION.SIDE_PROJECTS}>
       <div className="flex flex-col items-center m-8">
-        <AnchoredHeader
-          anchor={SECTION.SIDE_PROJECTS}
-          title="Outside of work"
-          subTitle="What I've recently learned and worked on"
-        />
+        <AnchoredHeader anchor={SECTION.SIDE_PROJECTS} title={t('projects.title')} subTitle={t('projects.sub-title')} />
       </div>
     </Section>
   );
