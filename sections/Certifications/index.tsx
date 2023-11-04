623647a977c48b292efc5e0f4aadde7d7352e271
Atsuhiro <atcheri@gmail.com>
2023-11-04 18:55:18 +0100

    feat: add sub-title to certificate block title

--- sections/Certifications/index.tsx
+++ sections/Certifications/index.tsx
@@ -12,7 +12,11 @@ const Certificates: FC = () => {
   return (
     <Section id={`${SECTION.CERTIFICATES}`} className="text-center py-20">
       <div className="flex flex-col items-center m-8">
-        <AnchoredHeader anchor={SECTION.CERTIFICATES} title={t('certificates')} subTitle={''} />
+        <AnchoredHeader
+          anchor={SECTION.CERTIFICATES}
+          title={t('certificates.title')}
+          subTitle={t('certificates.sub-title')}
+        />
       </div>
       <div className="grid grid-cols-1 gap-2 md:grid-cols-4 md:gap-2">
         <BlockchainCouncilCertificates />
