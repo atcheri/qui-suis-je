9a93b0ae678e98d913518c3d8ed1803dea4561ef
atcheri <atcheri@gmail.com>
2023-11-07 10:47:02 +0100

    feat: add blockchain architect certificate

--- sections/Certifications/BlockchainCouncilCertificates.tsx
+++ sections/Certifications/BlockchainCouncilCertificates.tsx
@@ -12,16 +12,22 @@ type BlockchainCouncilCertificate = {
 
 const blockchainCouncilCertificates: BlockchainCouncilCertificate[] = [
   {
-    title: 'Certified Blockchain Expert',
-    url: 'https://www.credential.net/b6e25b46-b91c-4905-abb8-b6d60f591a73#gs.0ktw2e',
-    badgeUrl: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/75455426',
-    imageUrl: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/75455426',
-  },
-  {
     title: 'Certified Blockchain Developer',
     url: 'https://www.credential.net/af804fdc-b4a1-4d4d-923c-6e01c38845c1#gs.0ivgys',
     badgeUrl: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/86180577',
     imageUrl: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/86180577',
+  },
+  {
+    title: 'Certified Blockchain Architect',
+    url: 'https://www.credential.net/cc3e35ac-43cd-47e0-90a5-33431fe0e80f#gs.037c7o',
+    badgeUrl: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/86368568',
+    imageUrl: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/86368568',
+  },
+  {
+    title: 'Certified Blockchain Expert',
+    url: 'https://www.credential.net/b6e25b46-b91c-4905-abb8-b6d60f591a73#gs.0ktw2e',
+    badgeUrl: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/75455426',
+    imageUrl: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/75455426',
   },
 ];
 
